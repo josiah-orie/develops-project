53c4ea1508d82d420c6bb7183eb45a6877d7b3bb
josiah-orie <josiahonuorie@gmail.com>
2024-07-08 09:50:09 +0100

    project commit

---
+++ src/main/java/com/project/develops_project/DevelopsProjectApplication.java
@@ -0,0 +1,13 @@
+package com.project.develops_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevelopsProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevelopsProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/develops_project/DevelopsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.develops_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevelopsProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
